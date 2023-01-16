5c50eb2732710016df8ff48e4f733ac42ac2af33
Alexey Fishler <amberdance@yandex.ru>
2023-01-16 16:43:15 +0300

    [fix] update dependencies

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,33 @@
-module.exports = {};
+process.env.VUE_APP_VERSION = require("./package.json").version;
+process.env.VUE_APP_TITLE = "Рейтинг управляющих компаний";
+process.env.VUE_APP_API_URL =
+  process.env.NODE_ENV == "development"
+    ? "http://nadzor26-polls/api"
+    : "http://polls.nadzor26.ru/api";
+
+module.exports = {
+  productionSourceMap: false,
+  lintOnSave: process.env.NODE_ENV !== "production",
+
+  chainWebpack: (config) =>
+    config.plugin("html").tap((args) => {
+      args[0].title = process.env.VUE_APP_TITLE;
+      return args;
+    }),
+
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "production") {
+      config.output.filename =
+        "js/[name].[contenthash:8].min.js" +
+        "?v=" +
+        process.env.VUE_APP_VERSION;
+      config.output.chunkFilename =
+        "js/[name].[contenthash:8].min.js" +
+        "?v=" +
+        process.env.VUE_APP_VERSION;
+    } else {
+      config.output.filename = "js/[name].js";
+      config.output.chunkFilename = "js/[name].js";
+    }
+  },
+};
