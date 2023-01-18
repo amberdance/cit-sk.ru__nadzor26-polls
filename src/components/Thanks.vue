1859ae76b9cfcefd6e13676a35041f6f4d78c020
Mikhail Kucherenko <106554488+alexeevch@users.noreply.github.com>
2023-01-18 11:09:16 +0300

    [refactoring] Thanks.vue styling

--- src/components/Thanks.vue
+++ src/components/Thanks.vue
@@ -1,16 +1,17 @@
 <template>
   <MainLayout>
-    <div :class="$style.wrapper">
-      <h1 style="text-align: center; text-transform: uppercase">
-        Благодарим за участие в опросе!
-      </h1>
-      <div class="text-left">
-        <p>
-          Опрос по качеству работы управляющих организаций окончен
-          {{ END_DATE }}. Результаты опроса будут опубликованы на сайте
-          управления Ставропольского края - государственной жилищной инспекции.
-          <a href="http://nadzor26.ru" target="_blank">www.nadzor26.ru</a>
-        </p>
+    <div class="container">
+      <div :class="$style.wrapper">
+        <h1 style="text-align: center">Благодарим за участие в опросе!</h1>
+        <div class="text-left">
+          <p>
+            Опрос по качеству работы управляющих организаций окончен
+            {{ END_DATE }}. Результаты опроса будут опубликованы на сайте
+            управления Ставропольского края - государственной жилищной
+            инспекции.
+            <a href="http://nadzor26.ru" target="_blank">www.nadzor26.ru</a>
+          </p>
+        </div>
       </div>
     </div>
   </MainLayout>
@@ -44,8 +45,14 @@ export default {
 .wrapper {
   display: flex;
   flex-direction: column;
-  margin-top: 5%;
   background-color: #ffffffd1;
-  padding: 0.5rem;
+  padding: 3rem;
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+p {
+  text-align: center;
+  padding-top: 1rem;
 }
 </style>
